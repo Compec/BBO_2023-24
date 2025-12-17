869981a4cc89af5a009c74c7347f44ae46a198e7
Cursed95 <utkuderman6@gmail.com>
2025-12-18 00:11:52 +0300

    oylama bitti

--- Oylama/firebase-config.js
+++ Oylama/firebase-config.js
@@ -4,15 +4,6 @@
 // Firebase Console'dan aldığın config bilgilerini buraya yapıştır
 // https://console.firebase.google.com/ → Project Settings → Your apps
 
- const firebaseConfig = {
-    apiKey: "AIzaSyDppvWCZaWMnJrXylSVMG9QOfJ-p7gOoyg",
-    authDomain: "flutter-acee7.firebaseapp.com",
-    projectId: "flutter-acee7",
-    storageBucket: "flutter-acee7.firebasestorage.app",
-    messagingSenderId: "784153859610",
-    appId: "1:784153859610:web:2144d9c6ccedfa62e163a3",
-    measurementId: "G-N2BKPYBVWL"
-  };
 
 
 /*selam
